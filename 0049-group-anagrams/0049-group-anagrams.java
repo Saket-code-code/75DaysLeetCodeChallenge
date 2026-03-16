b7fa5b5c60202f4315eb3cec9bfdd3d1c76b425c
Saket Gupta <114693208+Saket-code-code@users.noreply.github.com>
2026-03-16 19:03:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,26 +1,15 @@
-class Solution {
+public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> ans = new HashMap<>();
-
+        Map<String, List<String>> res = new HashMap<>();
         for (String s : strs) {
             int[] count = new int[26];
-
-            // Count frequency of each letter in the string
             for (char c : s.toCharArray()) {
                 count[c - 'a']++;
             }
-
-            StringBuilder sb = new StringBuilder();
-            for (int num : count) {
-                sb.append(num).append("#");
-            }
-            String key = sb.toString();
-            if (!ans.containsKey(key)) {
-                ans.put(key, new ArrayList<>());
-            }
-            ans.get(key).add(s);
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
         }
-
-        return new ArrayList<>(ans.values());        
+        return new ArrayList<>(res.values());
     }
 }
