4151da5cf596a97bb23fecb080dfa94c8a492448
Saket Gupta <114693208+Saket-code-code@users.noreply.github.com>
2026-05-11 19:53:10 +0530

    Time: 2 ms (3.69%), Space: 45.8 MB (5.04%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        Queue<Integer> sumQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        sumQueue.add(targetSum - root.val);
+
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            int currSum = sumQueue.poll();
+
+            if (node.left == null && node.right == null && currSum == 0) {
+                return true;
+            }
+
+            if (node.left != null) {
+                nodeQueue.add(node.left);
+                sumQueue.add(currSum - node.left.val);
+            }
+
+            if (node.right != null) {
+                nodeQueue.add(node.right);
+                sumQueue.add(currSum - node.right.val);
+            }
+        }
+
+        return false;
+    }
+}
